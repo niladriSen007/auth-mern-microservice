98657b125a3cf1994e07238f68b08e8aed20ecb3
Niladri <niladrisengit12@gmail.com>
2025-10-19 22:43:28 +0530

    feat: initialize database connection in server startup

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,19 @@
 import 'reflect-metadata';
 import { app } from './app.js';
 import { serverConfig } from './config/index.js';
+import { AppDataSource } from './config/data-source.js';
 
 const startServer = () => {
     try {
         const PORT = serverConfig?.PORT || 4000;
+        // Initialize the database connection
+        AppDataSource.initialize()
+            .then(() => {
+                console.log('Data Source has been initialized!');
+            })
+            .catch((error) => {
+                console.error('Error during Data Source initialization', error);
+            });
         app.listen(PORT, () => {
             console.log(`Server is running on http://localhost:${PORT}`);
         });
